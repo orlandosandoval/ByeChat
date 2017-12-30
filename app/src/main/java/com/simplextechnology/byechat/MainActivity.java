a6195a4bf1a491f48362fad15ba5c763b48d1b3e
Uber <ubermarin0911@gmail.com>
2017-12-29 21:40:42 -0500

    Initial commit

---
+++ app/src/main/java/com/simplextechnology/byechat/MainActivity.java
@@ -0,0 +1,36 @@
+package com.simplextechnology.byechat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    Spinner spinnerGender;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        fillGenderSpinner();
+    }
+
+
+    private void fillGenderSpinner(){
+        spinnerGender = findViewById(R.id.spinnerGender);
+        List genders   = new ArrayList();
+        genders.add("Seleccionar genero");
+        genders.add("Femenino");
+        genders.add("Masculino");
+        genders.add("Otro");
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this,android.R.layout.simple_dropdown_item_1line,genders);
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerGender.setAdapter(arrayAdapter);
+
+    }
+
+}
